c5e23c20f3d77deca39299e52b38966c5c76310f
Sara Wendland <info@rosebudsara.com>
2023-05-02 14:37:19 -0500

    added todos for later

--- index.js
+++ index.js
@@ -5,32 +5,60 @@ const fs = require('fs');
 const path = require('path');
 const shapes = require('./lib/shapes')
 
-const userShapeChoices = [
-    "Circle",
-    "Triangle",
-    "Square",
-       
-  ];
 
-  const logoQuestion = [
+
+  const logoQuestions = [
     {
         type: 'input', 
         name: 'letters', 
-        message: 'Enter three letters for your Logo'
+        message: 'Enter up to three letters for your Logo',
+        validate: textInput =>{
+            if (textInput && textInput.length <= 3){
+                return true
+            } else {
+                console.log("Please enter three max letters.")
+                return false
+            };}
     },
     {
         type: 'list', 
         name: 'shapeOfLogo', 
         message: 'What Shape Would you like your logo to be?',
-        choices: userShapeChoices,
+        choices: [
+            "Circle",
+            "Triangle",
+            "Square",   
+          ],
+        default: 'square',
     },
+    {
+        type: 'input', 
+        name: 'textColor', 
+        message: 'Enter a color or hexadecimal number for the color of the text on your logo',
+        default: 'white',
+    },
+    {
+        type: 'input', 
+        name: 'backgroundColor', 
+        message: 'Enter a color or hexadecimal number for the color of the background of your Logo',
+        default: "black",
+    },
+
+
   ];
 
 function init() {
-    inquirer.prompt(logoQuestion)
+    inquirer.prompt(logoQuestions)
     .then((answers) => {
-        console.log(answers);
+        console.log(answers); 
+    })
+
+    // finish this function with renderlogo funciton from shapes.js
+    .then((answers) => {
         
     })
 }
+
+// use fs to write to file-svg into examples folder
+
 init();
